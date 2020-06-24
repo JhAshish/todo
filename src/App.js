c540711d2c49b57dd67517cd278ebe9743d3f2c3
JhAshish <ashishjha432@gmail.com>
2020-06-24 08:33:29 +0530

    add todo prompts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import Todos from './components/Todos'
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+
+      },
+      {
+        id: 2,
+        title: 'Meeting',
+        completed: true
+
+      },
+      {
+        id: 3,
+        title: 'book reading',
+        completed: false
+
+      }
+    ]
+  }
+
+  render() {
+    console.log(this.state.todos)
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Todos todos={this.state.todos} />
     </div>
   );
+  }
 }
 
 export default App;
